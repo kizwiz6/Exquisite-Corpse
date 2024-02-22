cd08b26011253af2d85c0d0006d2f17a0bf0a575
Kieran Emery <kizwiz@hotmail.co.uk>
2024-02-22 15:22:46 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exquisite_Corpse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
